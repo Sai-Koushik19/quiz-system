f501bb3620cd75758d2b40878704c449157dbdbd
Sai-Koushik19 <saikoushik972@gmail.com>
2026-05-09 13:38:17 +0530

    fixed api route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL:
-   "https://quiz-system-xy79.onrender.com"
+  baseURL:
+    "https://quiz-system-xy79.onrender.com/api"
 });
 
 export default API;
